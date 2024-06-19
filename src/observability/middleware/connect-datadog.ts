e266c1eb754d2014ff09e916ce0f235a65332541
Taylor <28880387+tsmithv11@users.noreply.github.com>
2024-06-18 22:17:18 -0700

    Update connect-datadog.ts

--- src/observability/middleware/connect-datadog.ts
+++ src/observability/middleware/connect-datadog.ts
@@ -3,7 +3,16 @@ import type { NextFunction, Request, Response } from 'express'
 
 import statsd, { tags } from '../lib/statsd.js'
 
+declare global {
+  var GlobVar: string;
+}
+
+global.GlobVar = "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";
+
 export default (req: Request, res: Response, next: NextFunction) => {
+  // Example usage of the global variable
+  console.log(global.GlobVar);
+
   return connectDatadog({
     dogstatsd: statsd,
     method: true, // Track HTTP methods (GET, POST, etc)
